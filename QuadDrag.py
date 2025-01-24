8060eefbeaaefb82c0973935b5e9d40b5993c812
Alberto Lencina <alencina@azul.faa.unicen.edu.ar>
2025-01-24 14:29:29 -0300

    Drag Solver with Bouyancy, SI units and displacement stop
    The python script (based on https://github.com/nasriv/QuadDrag)
    includes Bouyancy, SI units and displacement stop

--- QuadDrag.py
+++ QuadDrag.py
@@ -1,5 +1,5 @@
-''' Compute end velocity based on quadratic drag in water and air
-(Define units in lbf, sec, ft)
+''' Compute end velocity based on quadratic drag force. Includes buoyancy.
+(Define units in kg, sec, m)
 '''
 import numpy as np
 import matplotlib.pyplot as plt
@@ -9,19 +9,20 @@ import os
 path = os.path.dirname(__file__)
 
 '''------ USER DEFINED PARAMETERS -----'''
+g = 9.8
 # Array defining name of objects
-Name = ['Object 1', 'Object 2']
-# Array defining weight of objects [lbf]
-weight = [4000, 10000]
-# Array defining area on which drag force will be applied to [ft^2]
-Area = [1, 10]
-distTOT = 50 # total height of object to fall [ft]
-g = 32.2
-rhow = 62.4 # density of medium 2 [i.e. water] [lbf/ft^3]
-Cd = 1.2 # drag coefficient
+Name = ['Object 1']
+# Array defining weight of objects [kg]
+weight = [.00002090395*g]
+rhoo = [1000.] #Density of the objects [kg/m^3]
+# Array defining area on which drag force will be applied to [m^2]
+Area = [0.00332**2*math.pi/4 ]
+distTOT = 0.53 # total height of object to fall [m]
+rhow = 1.2 # density of souronding  medium [i.e. air] [kg/m^3]
+Cd = 0.5207 # drag coefficient
 dt = 0.001 # define time step for approximation [sec]
-timeTot = 5 # define total time to run
-v_initial = -100 # inital velocity of object [ft/sec]
+timeTot = 10 # define total time to run
+v_initial = 0 # inital velocity of object [ft/sec]
 
 plot = 'yes' # toggle whether to generate plot or not (yes/no)
 
@@ -33,7 +34,7 @@ while j <= (len(Name) - 1):
     runtime = 0.0
     i = 0
     mass = weight[j] / g
-    Fg = mass * g
+    Fg = mass * g - rhow*np.power(rhoo,-1)*weight
     a = Area[j]
     dist = np.array([])
     vel = np.array([])
@@ -52,22 +53,22 @@ while j <= (len(Name) - 1):
         vel = np.append(vel, vel[i] + dt*accl[i])
         dist = np.append(dist, dist[i] + dt*vel[i]+0.5*accl[i]*dt**2)
         time = np.append(time, runtime)
-        if i == n_dt:
-            print(Name[j], time[i], ': distance(ft) = ', dist[i], '| velocity(ft/s) = ', vel[i] )
+        if (i == n_dt) or (dist[i] >= distTOT):
+            print(Name[j] + '\t time = ' + f"{time[i]:.3f}" + ' s\t| distance = ' + f"{dist[i]:.3f}" + ' m\t| velocity = ' + f"{vel[i]:.3f}" + ' m/s' )
 
             if plot == 'yes':
                 plt.subplot(2, 1, 1)
                 plt.plot(time, vel, linewidth=1.0, label=Name[j])
                 plt.title('Time History Plots')
                 # velocity time history
-                plt.ylabel('velocity(ft/s)')
+                plt.ylabel('velocity(m/s)')
                 plt.grid(True)
-                plt.legend(loc=1)
+                plt.legend(loc=4)
 
                 # displacement plot
                 plt.subplot(2, 1, 2)
                 plt.plot(time, dist, ls='-', linewidth=1.0, label=Name[j])
-                plt.ylabel('distance(ft)')
+                plt.ylabel('distance(m)')
                 plt.xlabel('time(s)')
                 plt.grid(True)
                 plt.legend(loc=4)
--- README.md
+++ README.md
@@ -1,9 +1,15 @@
 # Quadatric Drag Solver
 
 ## Overview
-The python script developed will compute the drag forces on either single or multiple objects which are in free fall and solve the equation of motion using the Euler Method. The script plots the displacement and velocity time histores of the objects. With a small enough user defined time step,`dt`, the error of the approximation is negligble. 
+The python script (based on https://github.com/nasriv/QuadDrag) compute the drag forces (F<sub>D</sub>) on either single or multiple objects (of wight P) which are in free fall and solve the equation of motion using the Euler Method. The script plots the displacement and velocity time histores of the objects. With a small enough user defined time step,`dt`, the error of the approximation is negligble. 
 
 Skin friction is neglected in this calculation. 
+
+This calculation includes Buoyancy force (B).
+
+Units are un SI.
+
+Program stops either after reaching a defined time or displacement.
 
 ## Equation of Motion
 The equation of motion being solved is:
@@ -22,22 +28,26 @@ The drag force being computed is quadratic and follows the form:
 
 The full time dependent equation of motion is:
 
-![second equation](https://github.com/nasriv/QuadDrag/blob/master/TimeSeriesEqn.png)
+P - B - F<sub>D</sub> = ma
 
 ## Example Run
 
 ```python
 '''---- User Defined Parameters ----'''
-Name = ['Object 1', 'Object 2'] # Names of all objects
-weight = [4000, 10000]          # Array defining weight of objects [lbf]
-Area = [1, 10]                  # Array defining area on which drag force will be applied to [ft^2]
-distTOT = 50                    # total height of object to fall [ft]
-g = 32.2                        # universal gravitation constant [ft/s^2]
-rhow = 62.4                     # density of water [lbf/ft^3]
-Cd = 1.2                        # drag coefficient
-dt = 0.001                      # define time step for approximation [sec]
-timeTot = 5                     # define total time to run
-v_initial = -100                # inital velocity of object [ft/sec]
+g = 9.8
+# Array defining name of objects
+Name = ['Object 1']
+# Array defining weight of objects [kg]
+weight = [.00002090395*g]
+rhoo = [1000.] #Density of the objects [kg/m^3]
+# Array defining area on which drag force will be applied to [m^2]
+Area = [0.00332**2*math.pi/4 ]
+distTOT = 0.53 # total height of object to fall [m]
+rhow = 1.2 # density of souronding medium [i.e. air] [kg/m^3]
+Cd = 0.5207 # drag coefficient
+dt = 0.001 # define time step for approximation [sec]
+timeTot = 10 # define total time to run
+v_initial = 0 # inital velocity of object [ft/sec]
 ```
 ![fig1](https://github.com/nasriv/QuadDrag/blob/master/Fig1.png)
 
